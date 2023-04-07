95865799602ba87990a4bf8cb81474ac46903b35
201905451 <up201905451@up.pt>
2023-04-07 16:55:05 +0100

    database update

--- database.sql
+++ database.sql
@@ -17,23 +17,21 @@ CREATE TABLE "Curso" (
 	UNIQUE(codigo, ano)
 );
 
-CREATE TABLE "Aluno" (
+CREATE TABLE "Colocado" (
     "id" INTEGER PRIMARY KEY,
 	"codigo" TEXT NOT NULL,
 	"nome" TEXT NOT NULL,
-	"12ano"	INTEGER NOT NULL,
-    "1011ano" INTEGER NOT NULL,
 	UNIQUE(codigo, nome)
 );
 
 CREATE TABLE "Candidatura" (
+	"id" INTEGER NOT NULL,
+	"codigo" TEXT NOT NULL,
+	"nome" TEXT NOT NULL,
 	"media"	TEXT NOT NULL,
 	"opcao" INTEGER NOT NULL,
-	"colocado" BOOLEAN NOT NULL,
-    "pi" INTEGER NOT NULL,
-	"curso" INTEGER NOT NULL,
-	"aluno" INTEGER NOT NULL,
-	FOREIGN KEY(curso) REFERENCES Curso(id),
-	FOREIGN KEY(aluno) REFERENCES Aluno(id),
-	PRIMARY KEY(curso, aluno)
+	"pi" INTEGER NOT NULL,
+	"12ano"	INTEGER NOT NULL,
+    "1011ano" INTEGER NOT NULL,
+	UNIQUE(codigo, nome)
 );
