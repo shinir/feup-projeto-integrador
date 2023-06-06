ab97fbb1546464ff536ba6e30ebfbff193d0b84d
AnaRita23 <cora.rita2002@gmail.com>
2023-06-06 03:21:28 +0100

    graficos praticamente a funcionar

--- app.py
+++ app.py
@@ -32,6 +32,8 @@ import subprocess
 import sqlite3
 import matplotlib.pyplot as plt
 
+from graphs import plot
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -39,7 +41,7 @@ def index():
     # Connect to the database and retrieve data from the 'curso' table
     conn = sqlite3.connect('db/database.db')
     cursor = conn.cursor()
-    cursor.execute("SELECT c.instituicao, c.ano, c.nome FROM Curso c WHERE c.id IN (SELECT curso FROM Candidatura)")
+    cursor.execute("SELECT c.instituicao, c.ano, c.nome, c.id FROM Curso c WHERE c.id IN (SELECT curso FROM Candidatura)")
     data = cursor.fetchall()
     cursor.close()
     conn.close()
@@ -51,14 +53,48 @@ def generate_graphs():
     selected_courses = request.form.getlist('courses')
 
     # Call the 'main' function to generate the graphs for the selected courses
-    main(selected_courses)
+    graph_filenames = main(selected_courses)
 
-    return redirect('/results')
+    return redirect('/results?graphs=' + ','.join(graph_filenames))
 
 @app.route('/results')
 def display_results():
+    # Retrieve the 'graphs' parameter from the query string
+    graph_filenames = request.args.get('graphs')
+
+    if not graph_filenames:
+        # Handle the case when no graphs were generated
+        return render_template('no_results.html')
+
+    # Split the filenames by comma
+    graph_filenames = graph_filenames.split(',')
+
+    # Create a list of graph dictionaries
+    graphs = []
+    for graph_filename in graph_filenames:
+        course_id = graph_filename.split('_')[1].split('.')[0]
+        conn = sqlite3.connect('db/database.db')
+        cursor = conn.cursor()
+        cursor.execute("SELECT codigo, nome FROM Curso WHERE id = ?", (course_id,))
+        course = cursor.fetchone()
+
+        if course:
+            course_code, course_name = course
+
+            print(f"Course Code: {course_code}, Course Name: {course_name}")
+            
+            graph_info = {
+                'title': f'Graph for {course_code} - {course_name}',
+                'image': graph_filename
+            }
+            graphs.append(graph_info)
+
+        # Close the database connection
+        cursor.close()
+        conn.close()
+
     # Render the 'results.html' template to display the generated graphs
-    return render_template('results.html')
+    return render_template('results.html', graphs=graphs)
 
 def main(selected_courses):
     # Connect to the database and retrieve data for the selected courses
@@ -66,47 +102,32 @@ def main(selected_courses):
     cursor = conn.cursor()
 
     # Initialize a list to store the generated graphs
-    graphs = []
+    graph_filenames = []
 
-    # Generate graphs for each selected course
+    # Generate graphs for the selected courses
+    plot(conn, cursor, selected_courses)  # Call plot with the selected_courses list
+
+    # Save the graph as a PNG file for each course
     for course_id in selected_courses:
-        # Retrieve course information
-        cursor.execute("SELECT codigo, nome FROM Curso WHERE id = ?", (course_id,))
+        query = "SELECT codigo, nome FROM Curso WHERE id = ?"
+        cursor.execute(query, (course_id,))
         course = cursor.fetchone()
 
         if course:
-            course_code, course_name = course
 
-            # Generate graph based on course data
-            # Example code: Generate a PNG file with a simple graph
-            x = [1, 2, 3, 4, 5]
-            y = [5, 4, 3, 2, 1]
-
-            plt.plot(x, y)
-            plt.xlabel('X')
-            plt.ylabel('Y')
-            plt.title(f'Graph for {course_code} - {course_name}')
-            
             # Save the graph as a PNG file in the 'static' folder
             graph_filename = f'graphs/graph_{course_id}.png'
             plt.savefig(graph_filename)
             plt.close()
 
-            # Create a dictionary to store the graph information
-            graph_info = {
-                'title': f'Graph for {course_code} - {course_name}',
-                'image': graph_filename
-            }
-
-            # Add the graph to the list of generated graphs
-            graphs.append(graph_info)
+            graph_filenames.append(graph_filename)
 
     # Close the database connection
     cursor.close()
     conn.close()
 
     # Return the list of generated graphs
-    return graphs
+    return graph_filenames
 
 
 if __name__ == '__main__':
--- graphs.py
+++ graphs.py
@@ -1,62 +1,32 @@
 import matplotlib.pyplot as plt
-# import plotly
-import csv
 
-def plotCSV(filepath, sigla):
-    for name, sig in zip(filepath,sigla):
-        with open(name) as csvfile:
-            reader = csv.reader(csvfile, delimiter=";")
-            # Read the header row
-            header = next(reader)
-            # Initialize lists to store data
-            x = []
-            y = []
-            order = 0
-            # Read each row of data and append to lists
-            for row in reader:
-                if int(row[4]) == 1:
-                    order+=1
-                    x.append(float(order))
-                    y.append(float(row[3]))
 
-        # Create the plot
-        plt.plot(x,y,label=sig)
-        plt.legend()
-    plt.grid(True, "both")
-    plt.xlabel("Número de Candidatos")
-    plt.ylabel("Média de candidatura")
-    plt.title('Candidatos em 1ª opção')
-    plt.show()
+def plot(conn, cursor, course_id_list):
+    # Create a new figure
+    plt.figure()
 
-def plot(conn, cursor):
-    query = "SELECT id, media, curso FROM candidatura"
-    cursor.execute(query)
+    # Iterate over the selected courses
+    for course_id in course_id_list:
+        query = "SELECT id, media, curso FROM candidatura WHERE curso = ?"
+        cursor.execute(query, (course_id,))
 
-    # Initialize dictionaries to store the grouped data
-    data_dict = {}
+        # Initialize lists to store the x and y values
+        x_values = []
+        y_values = []
 
-    # Fetch and process the results
-    for row in cursor:
-        x = row[0]
-        y = float(row[1])
-        group = row[2]
-        
-        if group not in data_dict:
-            data_dict[group] = {'x': [], 'y': []}
-        
-        data_dict[group]['x'].append(x)
-        data_dict[group]['y'].append(y)
+        # Fetch and process the results
+        for row in cursor:
+            x = row[0]
+            y = float(row[1])
 
-    # Create the plot
-    for group, data in data_dict.items():
-        plt.plot(data['x'], data['y'], label=group)
+            x_values.append(x)
+            y_values.append(y)
+
+        # Plot the data for the current course
+        plt.plot(x_values, y_values, label=f'Course ID: {course_id}')
 
     plt.xlabel("Número de candidatos")
     plt.ylabel("Média de candidatura")
     plt.title("Candidatos em 1ª opção")
     plt.legend()
     plt.show()
-
-    # Close the database connection
-    cursor.close()
-    conn.close()
