420ef3fd2acfefa527d2de7e19aad88f486a0761
Hugo <hfralmeida@gmail.com>
2023-06-20 02:10:23 +0100

    Função de colocados e candidatos

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ import subprocess
 import sqlite3
 import matplotlib.pyplot as plt
 
-from graphs import plot
+from graphs import plot_candidatos
+from graphs import plot_colocados
 
 app = Flask(__name__)
 
@@ -26,14 +27,14 @@ def candidatos():
 
 
 @app.route('/candidatos_graph', methods=['POST'])
-def candidatos_graph():
+def candidatos_graph():    
     selected_courses = request.form.getlist('courses')
 
-    # Call the 'main' function to generate the graphs for the selected courses
-    graph_filenames = main(selected_courses)
+    # Call the 'main' function to generate the graph for the selected courses
+    graph_filename = candidatos_plot(selected_courses)
+    return render_template('results.html', graph_url=graph_filename)
 
-    return redirect('/results?graphs=' + ','.join(graph_filenames))
-
+#
 @app.route('/colocados')
 def colocados():
     # Connect to the database and retrieve data from the 'curso' table
@@ -46,69 +47,54 @@ def colocados():
 
     return render_template('display_colocados.html', data=data)
 
+
 @app.route('/colocados_graph', methods=['POST'])
-def colocados_graph():
+def colocados_graph():    
     selected_courses = request.form.getlist('courses')
 
-    # Call the 'main' function to generate the graphs for the selected courses
-    graph_filenames = main(selected_courses)
+    # Call the 'main' function to generate the graph for the selected courses
+    graph_filename = colocados_plot(selected_courses)
+    return render_template('results.html', graph_url=graph_filename)
 
-    return redirect('/results?graphs=' + ','.join(graph_filenames))
+#
 
 
-@app.route('/results')
-def display_results():
-    # Retrieve the 'graphs' parameter from the query string
-    graph_filenames = request.args.get('graphs')
+from flask import send_from_directory 
+@app.route('/graphs/<filename>')
+def serve_dynamic_image(filename):
+    return send_from_directory('graphs', filename)
 
-    if not graph_filenames:
-        # Handle the case when no graphs were generated
-        return render_template('no_results.html')
 
-    # Split the filenames by comma
-    graph_filenames = graph_filenames.split(',')
 
-    # Create a list of graph dictionaries
-    graphs = []
-    for graph_filename in graph_filenames:
-        course_id = graph_filename.split('_')[1].split('.')[0]
-        conn = sqlite3.connect('db/database.db')
-        cursor = conn.cursor()
-        cursor.execute("SELECT codigo, nome FROM Curso WHERE id = ?", (course_id,))
-        course = cursor.fetchone()
-
-        if course:
-            course_code, course_name = course
-
-            print(f"Course Code: {course_code}, Course Name: {course_name}")
-            
-            graph_info = {
-                'title': f'Graph for {course_code} - {course_name}',
-                'image': graph_filename
-            }
-            graphs.append(graph_info)
-
-        # Close the database connection
-        cursor.close()
-        conn.close()
-
-    # Render the 'results.html' template to display the generated graphs
-    return render_template('results.html', graphs=graphs)
-
-def main(selected_courses):
+def candidatos_plot(selected_courses):
     # Connect to the database and retrieve data for the selected courses
     conn = sqlite3.connect('db/database.db')
     cursor = conn.cursor()
 
     # Generate graphs for the selected courses
-    plot(conn, cursor, selected_courses, 'graphs/all_courses.png')  # Call plot with the selected_courses list
+    plot_candidatos(conn, cursor, selected_courses, 'graphs/all_courses.png')  # Call plot with the selected_courses list
 
     # Close the database connection
     cursor.close()
     conn.close()
 
     # Return the list of generated graphs
-    return ['graphs/all_courses.png']
+    return 'graphs/all_courses.png'
+
+def colocados_plot(selected_courses):
+    # Connect to the database and retrieve data for the selected courses
+    conn = sqlite3.connect('db/database.db')
+    cursor = conn.cursor()
+
+    # Generate graphs for the selected courses
+    plot_colocados(conn, cursor, selected_courses, 'graphs/all_courses.png')  # Call plot with the selected_courses list
+
+    # Close the database connection
+    cursor.close()
+    conn.close()
+
+    # Return the list of generated graphs
+    return 'graphs/all_courses.png'
 
 
 if __name__ == '__main__':
--- graphs.py
+++ graphs.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 
 
-def plot(conn, cursor, course_id_list, save_filename):
+def plot_candidatos(conn, cursor, course_id_list, save_filename):
     # Create a new figure
     plt.figure()
 
@@ -27,7 +27,39 @@ def plot(conn, cursor, course_id_list, save_filename):
 
     plt.xlabel("Número de candidatos")
     plt.ylabel("Média de candidatura")
-    plt.title("Candidatos em 1ª opção")
+    plt.title("Candidatos")
+    plt.legend()
+    plt.savefig(save_filename)
+    plt.close()
+
+
+def plot_colocados(conn, cursor, course_id_list, save_filename):
+    # Create a new figure
+    plt.figure()
+
+    # Iterate over the selected courses
+    for course_id in course_id_list:
+        query = "SELECT Candidatura.id, Candidatura.media, Candidatura.curso FROM Colocado JOIN Candidatura ON Colocado.nome = Candidatura.nome AND candidatura.curso = ?"
+        cursor.execute(query, (course_id,))
+
+        # Initialize lists to store the x and y values
+        x_values = []
+        y_values = []
+
+        # Fetch and process the results
+        for row in cursor:
+            x = row[0]
+            y = float(row[1])
+
+            x_values.append(x)
+            y_values.append(y)
+
+        # Plot the data for the current course
+        plt.plot(x_values, y_values, label=f'Course ID: {course_id}')
+
+    plt.xlabel("Número de candidatos")
+    plt.ylabel("Média de candidatura")
+    plt.title("Colocados")
     plt.legend()
     plt.savefig(save_filename)
     plt.close()
